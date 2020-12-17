92c8f5b6887a2bc26fbbe0f9604fc4d8dc491288
DanielKotev <Danielkotev@gmail.com>
2020-12-17 19:07:44 +0200

    1

--- sortingAnimation/BubbleSort.cs
+++ sortingAnimation/BubbleSort.cs
@@ -10,20 +10,25 @@ namespace sortingAnimation
 
     public class BubbleSort
     {
+        
         public static void Sort(int[] array, ref int j)
         {
             
             int temp;
             for (int i=0; i < array.Length - 1; i++)
             {
+
                 for (; j < array.Length - i - 1;)
                 {
+
                     if (array[j] > array[j + 1])
                     {
                         temp = array[j + 1];
+                        
                         array[j + 1] = array[j];
                         array[j] = temp;
                     }
+
                     break;
                 }
                 if (j == array.Length)
--- sortingAnimation/Form1.cs
+++ sortingAnimation/Form1.cs
@@ -16,17 +16,24 @@ namespace sortingAnimation
         static int j = 0;
         const int num_values = 10;
         public int[] Numbers = new int[num_values];
+        public bool Selected = false;
+        
+        public Color[] colors = new Color[]
+        {
+            Color.Blue,
+            Color.Red
+        };
         public Form1()
         {
             InitializeComponent();
-            label_pic.Invalidate();
-            //typeof(PictureBox).InvokeMember("DoubleBuffered",
-            //    BindingFlags.SetProperty |
-            //    BindingFlags.Instance |
-            //    BindingFlags.NonPublic,
-            //    null,
-            //    label_pic,
-            //    new object[] { true });
+          
+            typeof(PictureBox).InvokeMember("DoubleBuffered",
+                BindingFlags.SetProperty |
+                BindingFlags.Instance |
+                BindingFlags.NonPublic,
+                null,
+                label_pic,
+                new object[] { true });
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -34,67 +41,19 @@ namespace sortingAnimation
         }
         private void paint()
         {
-
-        }
-        private void Generate_Click(object sender, EventArgs e)
-        {
-            j = 0;
-            label_pic.Controls.Clear();
-            FormBorderStyle = FormBorderStyle.FixedDialog;
-
-            Random rand = new Random();
-           
-            int wid = label_pic.ClientSize.Width / num_values;
-            for (int i = 0; i < num_values; i++)
-            {
-                int value = rand.Next(1, 96);
-                Numbers[i] = value;
-                // Make a label to be the histogram.
-                Label label_hist = new Label();
-                label_hist.Parent = label_pic;
-                label_hist.BackColor = Color.Blue;
-                label_hist.BorderStyle = BorderStyle.FixedSingle;
-                label_hist.Width = wid;
-                label_hist.Height =
-                    (int)(label_pic.ClientSize.Height * value / 100f);
-                label_hist.Left = i * wid;
-                label_hist.Top = label_pic.ClientSize.Height - label_hist.Height;
-
-
-                // Make a label to display the value.
-                Label label_value = new Label();
-                label_value.Parent = label_pic;
-                label_value.BackColor = Color.Transparent;
-                label_value.Text = value.ToString();
-                label_value.TextAlign = ContentAlignment.TopCenter;
-                label_value.Left = label_hist.Left;
-                label_value.Width = label_hist.Width;
-                label_value.Height = 15;
-                label_value.Top = label_hist.Top - label_value.Height;
-            }
-            label_pic.Invalidate();
-        }
-
-        private void Next_Click(object sender, EventArgs e)
-        {
-            
-            BubbleSort.Sort(Numbers,ref j);
-            j++;
-            label_pic.Controls.Clear();
             int wid = label_pic.ClientSize.Width / num_values;
             for (int i = 0; i < num_values; i++)
             {
                 // Make a label to be the histogram.
                 Label label_hist = new Label();
                 label_hist.Parent = label_pic;
-                label_hist.BackColor = Color.Blue;
+                label_hist.BackColor = colors[0];
                 label_hist.BorderStyle = BorderStyle.FixedSingle;
                 label_hist.Width = wid;
                 label_hist.Height =
                     (int)(label_pic.ClientSize.Height * Numbers[i] / 100f);
                 label_hist.Left = i * wid;
                 label_hist.Top = label_pic.ClientSize.Height - label_hist.Height;
-
 
                 // Make a label to display the value.
                 Label label_value = new Label();
@@ -107,9 +66,29 @@ namespace sortingAnimation
                 label_value.Height = 15;
                 label_value.Top = label_hist.Top - label_value.Height;
             }
+        }
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            j = 0;
+            label_pic.Controls.Clear();
+            FormBorderStyle = FormBorderStyle.FixedDialog;
 
+            Random rand = new Random();
+            for (int i = 0; i < num_values; i++)
+            {
+                int value = rand.Next(1, 96);
+                Numbers[i] = value;
+            }
+            paint();
+            label_pic.Invalidate();
         }
 
-     
+        private void Next_Click(object sender, EventArgs e)
+        { 
+            BubbleSort.Sort(Numbers,ref j);
+            j++;
+            label_pic.Controls.Clear();
+            paint();
+        }
     }
 }
